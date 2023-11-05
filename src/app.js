0dbc79f3b578857d511136ade14291f6cfa00f67
alondragerke <141412967+alondragerke@users.noreply.github.com>
2023-11-05 12:00:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let sujeto = [
+    "mis loros",
+    "las luciérnagas",
+    "un grupo de patos",
+    "los extraterrestres",
+    "mis vecinos"
+  ];
+  let accion = [
+    "robaron mi lámpara",
+    "organizaron una procesión",
+    "dieron un discurso político",
+    "me extorsionaron con navajas",
+    "me robaron el móvil"
+  ];
+  let tiempo = [
+    "ayer",
+    "la semana pasada",
+    "hace una hora",
+    "hace unos días",
+    "hace dos días"
+  ];
+  let problema = [
+    "así que no he podido volver a casa.",
+    "así que no he podido llamarte.",
+    "así que no he podido salir de casa.",
+    "así que no encontré el regalo perfecto para tu cumpleaños.",
+    "así que no puedo ir."
+  ];
+
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let tiempoIndex = Math.floor(Math.random() * tiempo.length);
+  let problemaIndex = Math.floor(Math.random() * problema.length);
+
+  return (
+    sujeto[sujetoIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    tiempo[tiempoIndex] +
+    " " +
+    problema[problemaIndex]
+  );
 };
